9ae1c013c0652c1226a8516adc2a24b2c1b4d0d8
prashantverma22 <prashantverma.vn@gmail.com>
2020-04-26 18:28:29 +0530

    Add files via upload

---
+++ build/precache-manifest.25f554b0c4739ce2f71031cecee05fcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "374916637c51420c1ea4",
+    "url": "/static/js/main.b2c48314.chunk.js"
+  },
+  {
+    "revision": "fa6d91c853d34fbd4ce5",
+    "url": "/static/js/2.e3caa356.chunk.js"
+  },
+  {
+    "revision": "374916637c51420c1ea4",
+    "url": "/static/css/main.6273b541.chunk.css"
+  },
+  {
+    "revision": "15ec89f39578dec6e5a94cb0cc01b398",
+    "url": "/index.html"
+  }
+];
